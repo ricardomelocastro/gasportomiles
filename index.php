ac010b64cf1dd0377e09f47ea8f9b564c07748de
ricardomelocastro <ricardo.melo.castro@gmail.com>
2016-06-19 22:29:12 +0100

    man

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Doar Milhas G.A.S.Porto</title>
+		<meta charset="UTF-8">
+		<!--<link rel="stylesheet" type="text/css" href="css/milhas.css">-->
+
+		
+		<style>
+			img{
+				max-width: 100%;
+				max-height: 100%;
+			   
+			}
+
+			.fs-image{
+				max-width: 100%;
+				max-height: 100%;
+			   
+			}
+
+			.share_button{
+			   cursor:pointer;
+			}
+			
+		</style>
+</head>
+<body>
+   
+   <div id="fb-root"></div>
+
+   <div class="main-container">
+      <!--<div id="inicio" class="fs-image">
+         <img src="https://paginas.fe.up.pt/~ei09126/gasporto/imagens-01.png">
+      </div>-->
+      <div id="inicio" class="fs-image">
+         <img  src="img/passo1-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo2-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo3-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo4-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo5-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo6-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo7-01.png">
+      </div>
+      <div class="fs-image">
+         <img  src="img/passo8-01.png">
+      </div>
+      
+      
+      <!--
+      <div>
+      <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/gasporto" onclick="javascript:void window.open('https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/gasporto','1426690556641','width=500,height=300,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img  src="https://paginas.fe.up.pt/~ei09126/gasporto/imagens-12.png"></a>
+      </div>
+   
+      
+      <div class="fs-image">
+         <img  src="https://paginas.fe.up.pt/~ei09126/gasporto/imagens-13.png">
+      </div>
+      
+      <div class="fs-image">
+         <a href="#inicio" onclick="document.getElementById('inicio').scrollIntoView();"><img src="https://paginas.fe.up.pt/~ei09126/gasporto/imagens-14.png"></a> 
+      </div> -->
+
+</body>
+   
+    <!--  <script>
+      window.fbAsyncInit = function() {
+      FB.init({appId: '190322544333196' , status: true, cookie: true,
+      xfbml: true});
+      };
+      (function() {
+      var e = document.createElement('script'); e.async = true;
+      e.src = document.location.protocol +
+      '//connect.facebook.net/en_US/all.js';
+      document.getElementById('fb-root').appendChild(e);
+      }());
+      </script>
+      
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+      
+      <script>
+      $(document).ready(function(){
+         $('#share_button').click(function(e){
+         e.preventDefault();
+         FB.ui(
+         {
+         method: 'feed',
+         name: 'This is the content of the "name" field.',
+         link: 'https://www.facebook.com/gasporto/app/190322544333196/',
+         picture: 'https://paginas.fe.up.pt/~ei09126/gasporto/felicidade.jpg',
+         caption: 'Top 3 reasons why you should care about your finance',
+         description: "What happens when you don't take care of your finances? Just look at our country -- you spend irresponsibly, get in debt up to your eyeballs, and stress about how you're going to make ends meet. The difference is that you don't have a glut of taxpayers…",
+         message: ""
+         });
+         });
+         });
+         </script>-->
+
+
+</html>
